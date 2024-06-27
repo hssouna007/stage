82937750600edc5bdddf4709737026ce4cab3174
hssouna007 <161390741+hssouna007@users.noreply.github.com>
2024-06-27 17:54:40 +0200

    Delete database directory

--- database/src/main/java/com/springdata/ecommerce/service/ProductService.java
+++
@@ -1,8 +0,0 @@
-package com.springdata.ecommerce.service;
-
-import com.springdata.ecommerce.models.Product;
-
-public interface ProductService {
-    Product saveProduct(Product product);
-    // Declare other CRUD operations as needed
-}
