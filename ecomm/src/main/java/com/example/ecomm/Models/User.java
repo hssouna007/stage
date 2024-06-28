588218be349f15e68feae344efd4af76a9f21525
hssouna007 <161390741+hssouna007@users.noreply.github.com>
2024-06-28 18:36:03 +0200

    Add files via upload

---
+++ ecomm/src/main/java/com/example/ecomm/Models/User.java
@@ -0,0 +1,33 @@
+package com.example.ecomm.Models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String name;
+
+    // New attributes added
+    @Column(nullable = false) // Assuming age is mandatory
+    @Digits(fraction = 0, integer = 3, message = "Age must be a positive integer.") // Validate age
+    private Integer age;
+
+    @Column(nullable = false) // Assuming job title is mandatory
+    private String jobTitle;
+
+    @Column(unique = true, nullable = false) // Assuming phone numbers are unique and mandatory
+    private String phoneNumber;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Basket> baskets = new ArrayList<>();
+    // Constructor, getters, and setters omitted for brevity
+}
